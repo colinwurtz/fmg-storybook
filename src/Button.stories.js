e9e452578850661e695464ac39334e85fab5ba06
colinwurtz <colinwurtz@gmail.com>
2018-12-16 16:15:01 -0600

    convert button to typescript and add boolean knob

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,17 +1,33 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+
+// storiesOf("Button", module)
+//   .addWithJSX(
+//     "with background",
+//     withInfo(
+//       `some descriptive text here. Maybe a description of the component`
+//     )(() => <Button bg="palegoldenrod">Hello world</Button>)
+//   )
+//   .addWithJSX("with background 2", () => (
+//     <Button bg={text("bg", "blue")}>Hello world2</Button>
+//   ));
+
+import { text, boolean } from "@storybook/addon-knobs/react";
 import { Button } from "./Button";
-
-import { text } from "@storybook/addon-knobs/react";
-
 storiesOf("Button", module)
   .addWithJSX(
     "with background",
-    withInfo(
-      `some descriptive text here. Maybe a description of the component`
-    )(() => <Button bg="palegoldenrod">Hello world</Button>)
+    withInfo(`
+      description
+      
+      ~~~js
+      <Button>slkdjslkdj</Button>
+      ~~~
+      `)(() => <Button bg="palegoldenrod">Hello world</Button>)
   )
   .addWithJSX("with background 2", () => (
-    <Button bg={text("bg", "blue")}>Hello world2</Button>
+    <Button disabled={boolean("disabled", false)}>
+      {text("text", "Hello world")}
+    </Button>
   ));
