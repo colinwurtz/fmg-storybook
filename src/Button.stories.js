bdc91bff3cc998ca051854e913e1a7577a733b15
colinwurtz <colinwurtz@gmail.com>
2018-12-16 16:22:25 -0600

    add docgen addon to auto populate prop types in table

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,24 +1,14 @@
 import React from "react";
+import { wInfo } from "./utils";
+
 import { storiesOf } from "@storybook/react";
-import { withInfo } from "@storybook/addon-info";
-
-// storiesOf("Button", module)
-//   .addWithJSX(
-//     "with background",
-//     withInfo(
-//       `some descriptive text here. Maybe a description of the component`
-//     )(() => <Button bg="palegoldenrod">Hello world</Button>)
-//   )
-//   .addWithJSX("with background 2", () => (
-//     <Button bg={text("bg", "blue")}>Hello world2</Button>
-//   ));
-
-import { text, boolean } from "@storybook/addon-knobs/react";
 import { Button } from "./Button";
+import { text, boolean } from "@storybook/addon-knobs/react";
+
 storiesOf("Button", module)
   .addWithJSX(
     "with background",
-    withInfo(`
+    wInfo(`
       description
       
       ~~~js
